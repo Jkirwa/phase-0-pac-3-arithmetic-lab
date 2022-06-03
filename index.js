676a05105097905b7e502f1786d1e11c368f3f53
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-03 21:22:11 +0300

    commit tree 8689e4a7c37a920b66828db739b26a8d1747a6a8

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a,b){
+return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+
+}
+function divide(a,b){
+    return a/b;
+
+}
+function increment(n){
+
+return ++n;
+}
+function decrement(n){
+
+    return --n;
+    }
+  function makeInt(n){
+    return n;
+}
+
+
+function preserveDecimal(n){
+    return n;
+}
+
+
+function preserveDecimal(n){
+    return  parseFloat(n);
+}
+
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+    
+
+  
+
+
